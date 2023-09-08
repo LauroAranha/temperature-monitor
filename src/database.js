f378002cf4cb3a3c639291e041f95fa3c8723e8c
Lauro Aranha <84295347+LauroAranha@users.noreply.github.com>
2023-09-07 23:39:46 -0300

    Update database.js

--- src/database.js
+++ src/database.js
@@ -7,6 +7,7 @@ console.log(process.env.POSTGRES_DATABASE)
 const sequelize = new Sequelize(process.env.POSTGRES_DATABASE, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
   host: process.env.POSTGRES_HOST,
   dialect: "postgres",
+  dialectModule: pg,
   dialectOptions: {
     ssl: {
       require: true
